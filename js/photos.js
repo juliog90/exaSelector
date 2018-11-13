781739d38078a410ab27ee43f11250bf99a2ed11
Julio Gastelum <tjuliogastelum90@gmail.com>
2018-11-13 15:02:28 -0800

    flechas rotas

--- js/photos.js
+++ js/photos.js
@@ -52,8 +52,24 @@ function photos(photos) {
     row.appendChild(column);
   }
   console.log(row);
-
   tabla.appendChild(row);
+    
+  // agregamos las flechas 
+  // body
+  let selector = document.getElementById('main');
+  // flechas
+  let left = document.createElement('img');
+  let right = document.createElement('img');
+  left.id = 'larrow';
+  left.src = 'images/left.png';
+  left.addEventListener('click', changueLeft, false);
+        
+  right.id = 'rarrow';
+  right.src = 'images/right.png';
+  right.addEventListener('click', changueRight, false);
+
+  selector.appendChild(left);
+  selector.appendChild(right);
 }
 
 // crea cada imagen
@@ -64,6 +80,7 @@ function createPhotoColumn(id) {
   image.src = 'images/' + current.imagen;
   image.width = 64;
   image.height = 64;
+  image.id = current.id;
   // aqui se agrega el evento que cambia el src de un elemento por otro en este caso del elemento seleccionado a la imagen inferior
   image.addEventListener("click", function() { changuePhoto(this) }, false);
   let column = document.createElement('td');
@@ -81,6 +98,51 @@ function getCurrent(id)
 }
 
 function changuePhoto(selectImg) {
-  let showPhoto = document.getElementById('bigPhoto');
+  let showPhoto = document.querySelector('.laImagen');
+  console.log(showPhoto);
   showPhoto.src = selectImg.src;
+  showPhoto.id = selectImg.id;
 }
+
+function changueLeft() {
+  let showPhoto = document.querySelector('.laImagen');
+  console.log(showPhoto);
+  let currentId = showPhoto.id;
+  showPhoto.src = getPreviousc(currentId);
+}
+
+function getNextSrc(currId) {
+  if(currId == photosData.length) {
+    console.log(photosData.length);
+    // si es la ultima regresa 1
+    return 'images/' + photosData[0].imagen;
+  }
+
+  photosData.forEach(function(photo) {
+    if(photo.id == currId) {
+      let nextImgSrc = 'images/' + photo.imagen; 
+      return nextImgSrc;
+    }
+  });
+}
+
+function changueRight() {
+  let showPhoto = document.querySelector('.laImagen');
+  let currentId = showPhoto.id;
+  showPhoto.src = getNextSrc(currentId);
+}
+
+function getPreviousSrc(currId) {
+  if(currId == 1) {
+    // si es la primera regresa la ultima
+    return 'images/' + photosData[photosData.length - 1].imagen;
+  }
+
+  photosData.forEach(function(photo) {
+    if(photo.alt == currId) {
+      let nextImgSrc = 'images/' + photo.imagen; 
+      return nextImgSrc;
+    }
+  });
+}
+  
